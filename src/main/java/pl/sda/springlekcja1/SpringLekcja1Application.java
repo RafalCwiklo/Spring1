5703b6b14116c57758110be2ada0e1bc48030ca6
RafalCwiklo <rafalcwiklo@gmail.com>
2023-02-02 18:34:44 +0100

    SpringLekcja1Application

---
+++ src/main/java/pl/sda/springlekcja1/SpringLekcja1Application.java
@@ -0,0 +1,13 @@
+package pl.sda.springlekcja1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLekcja1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLekcja1Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sda/springlekcja1/SpringLekcja1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.springlekcja1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLekcja1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
